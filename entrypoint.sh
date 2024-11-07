f06914868ae4845fddab9a7dd69d4cc35dca995d
Sirirach <srr@iixp.co.th>
2024-11-07 23:54:00 +0700

    comment auto unseal

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 
 # Start Vault in server mode
-vault server -config=/vault/config/vault-config.hcl &
+vault server -config=/vault/config/vault-config.hcl
 
-# Wait for Vault to start
-sleep 5
+# # Wait for Vault to start
+# sleep 5
 
-# Unseal Vault using the environment variable
-if [ -z "$UNSEAL_KEY" ]; then
-    echo "UNSEAL_KEY environment variable is not set."
-    exit 1
-fi
+# # Unseal Vault using the environment variable
+# if [ -z "$UNSEAL_KEY" ]; then
+#     echo "UNSEAL_KEY environment variable is not set."
+#     exit 1
+# fi
 
-vault operator unseal "$UNSEAL_KEY"
+# vault operator unseal "$UNSEAL_KEY"
 
-# Authenticate with Vault using the root token from the environment variable
-if [ -z "$ROOT_TOKEN" ]; then
-    echo "ROOT_TOKEN environment variable is not set."
-    exit 1
-fi
+# # Authenticate with Vault using the root token from the environment variable
+# if [ -z "$ROOT_TOKEN" ]; then
+#     echo "ROOT_TOKEN environment variable is not set."
+#     exit 1
+# fi
 
-vault login "$ROOT_TOKEN"
+# vault login "$ROOT_TOKEN"
 
-# Optionally: Create a function to save the current configuration
-save_config() {
-    # This command will retrieve the current configuration and save it
-    vault status -format=json > /vault/config/vault-status.json
-    echo "Vault configuration saved."
-}
+# # Optionally: Create a function to save the current configuration
+# save_config() {
+#     # This command will retrieve the current configuration and save it
+#     vault status -format=json > /vault/config/vault-status.json
+#     echo "Vault configuration saved."
+# }
 
-# Call the function to save the configuration
-save_config
+# # Call the function to save the configuration
+# save_config
 
-# Keep the script running to allow Vault to serve requests
-wait
+# # Keep the script running to allow Vault to serve requests
+# wait
